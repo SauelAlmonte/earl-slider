e26196e77913d0d7aeb96ab10c189b422060be8a
Salmonte <almontesauel@gmail.com>
2022-10-19 22:35:16 -0400

    Earl's Slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // content: ["./dist/**/*.{html,js}"],
-  content: ["./dist/**/*.{html,js}"],
+  content: [
+    "./public/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
