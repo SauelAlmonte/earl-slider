8ace7301645e7d70e8a29929fce218ca960755f8
Salmonte <almontesauel@gmail.com>
2022-10-26 15:01:27 -0400

    Earls Modal

--- public/app.js
+++ public/app.js
@@ -39,6 +39,7 @@ window.addEventListener('click', function (e) {
     // Ternary operator
   	// : = Else , ? = If
     // console.log(e.target === formModal ? "this is false" : "this is true")
+    //  Better idiomatic way of writing algorithims
     (e.target === formModal) ? formModal.classList.add('hidden') : false
 })
 
