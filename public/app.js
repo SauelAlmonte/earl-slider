545c0248af96bea580e9e84126327e63ec3981ee
Salmonte <almontesauel@gmail.com>
2022-10-26 14:41:56 -0400

    Earls Modal

--- public/app.js
+++ public/app.js
@@ -29,13 +29,13 @@ tableMenu.addEventListener('click', function() {
     // console.log(document.body)
 })
 
-window.addEventListener('click', function (e) {
-    if (e.target === formModal) {
-        formModal.classList.add('hidden')
-    } else {
-        return false
-    }
-    console.log(e.target)
-})
+// window.addEventListener('click', function (e) {
+//     if (e.target === formModal) {
+//         formModal.classList.add('hidden')
+//     } else {
+//         return false
+//     }
+//     console.log(e.target)
+// })
 
 // Method to write the above algorithm shorter
